15711bf16fdf0e2cb9a53dd068ca9ef84cf29c14
skvl00 <s21920002@student.unklab.ac.id>
2022-10-31 07:36:50 +0800

    Ezra

---
+++ Front-endwebdev/Sakul, Esra Andika Celfira/exercise 1/Exercise 1.js
@@ -0,0 +1,59 @@
+//Sakul, Esra Andika Celfira
+//Front-end Web Development A
+
+//concat
+let textConcat1 = "Saya";
+let textConcat2 = " tampan kan ?";
+let resultConcat = textConcat1.concat(textConcat2);
+console.log(resultConcat);
+
+
+//includes
+let textI = "exercise front end";
+let resultI = textI.includes("exercise");
+console.log(resultI);
+
+//another condition includes
+let resultI2 = textI.includes("kelas");
+console.log(resultI2);
+
+//length
+let numbers = [29, , 08, 01];
+console.log(numbers.length);
+
+//split
+let textsplit = "aku tresno karo koe";
+let word = [2];
+console.log(word);
+
+//slice
+const str = 'Love Coding';
+const substr = str.slice(6);
+console.log(substr);
+
+
+//substring
+let textSubstring = "world!";
+let resultsubstring = textSubstring.substring(1, 4);
+console.log(resultsubstring);
+
+
+//toLowerCase
+let text = "TERIMA KASIH";
+let resultlc = text.toLowerCase();
+console.log(resultlc);
+
+//toUpperCase
+let textUpperCase = "unklab";
+let resultuc = textUpperCase.toUpperCase();
+console.log(resultuc);
+
+//trim
+let textTrim = "            Jokowi            ";
+let resulttrim = textTrim.trim();
+console.log(resulttrim);
+
+//valueof
+let textvo = "Fakultas Ilmu Komputer";
+let resultvo = textvo.valueOf();
+console.log(resultvo);
